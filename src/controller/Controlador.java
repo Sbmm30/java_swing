88ba7f65e004290a0239cd6c20321859abb60904
Sebastian Mori <mori.sebastian@outlook.com>
2023-04-30 21:02:13 -0500

    creacion del codigo de la clase controlador

--- src/controller/Controlador.java
+++ src/controller/Controlador.java
@@ -4,10 +4,46 @@
  */
 package controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import model.Modelo;
+import view.Vista;
+
 /**
  *
- * @author DARLYN
+ * @author @authorsbmm30 https://github.com/sbmm30
  */
-public class Controlador {
-    
+public class Controlador implements ActionListener {
+
+    private Vista view;
+    private Modelo model;
+
+    public Controlador(Vista view, Modelo model) {
+        this.view = view;
+        this.model = model;
+        this.view.btnCalcular.addActionListener(this);
+        this.view.btnNuevo.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        model.setCategoria(view.cbCategoria.getSelectedIndex());
+        model.setPromedio(Integer.parseInt(view.tfPromedio.getText()));
+
+        if (e.getSource() == view.btnCalcular) {
+            model.ejecutarOperaciones();
+            view.tfPensionActual.setText(String.valueOf(model.getPensionActual()));
+            view.tfPensionNueva.setText(String.valueOf(model.getPensionNueva()));
+            view.tfDescuento.setText(String.valueOf(model.getDescuento()));
+        } else if (e.getSource() == view.btnNuevo) {
+            view.cbCategoria.setSelectedIndex(0);
+            view.tfPromedio.setText(null);
+            view.tfPensionActual.setText(null);
+            view.tfPensionNueva.setText(null);
+            view.tfDescuento.setText(null);
+        } else {
+
+        }
+    }
+
 }
